33ebd48034ad7e71e3392432963cbd12ffaa4cd3
maf29 <73944513+maf29@users.noreply.github.com>
2022-09-27 12:32:29 +0200

    compila

--- src/epsevg/upc/edu/c2223/PruebaRobot.java
+++ src/epsevg/upc/edu/c2223/PruebaRobot.java
@@ -4,10 +4,29 @@
  */
 package epsevg.upc.edu.c2223;
 
+import robocode.HitByBulletEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
 /**
  *
  * @author UX431F
  */
-public class PruebaRobot {
+public class PruebaRobot extends Robot{
+    public void run(){
+        turnLeft(getHeading());
+        while(true){
+            ahead(1000);
+            turnRight(90);
+        }
+    }
+    
+    public void onScannedRobot(ScannedRobotEvent e){
+        fire(1);
+    }
+    
+    public void onHitByBullet(HitByBulletEvent e){
+        turnLeft(180);
+    }
     
 }
